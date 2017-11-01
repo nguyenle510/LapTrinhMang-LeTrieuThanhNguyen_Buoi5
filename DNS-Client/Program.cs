fc3ec387a66f677e484acdfa87d60ac00c10040b
Nguyen Le <trieunguyen30@gmail.com>
2017-11-01 15:52:57 +0700

    commit 1

---
+++ DNS-Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using DNSClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNS_Client
+{
+    class Program
+    {
+        private static dnsclient dnsClient;
+        static void Main(string[] args)
+        {
+            dnsClient = new dnsclient();
+            dnsClient.requestResolve("https://google.com");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ DNS-Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using DNSServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNS_Server
+{
+    class Program
+    {
+        private static named named;
+        static void Main(string[] args)
+        {
+            named = new named();
+            while (!false)
+            {
+                named.startListening();
+            }
+        }
+    }
+}
